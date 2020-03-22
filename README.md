792c343f7cffc245e50325e957d0f0a28561a07f
Mihai Oprea <mihai.oprea@gmail.com>
2020-03-22 12:14:37 +0100

    also a webserver. readme still needs to be updated

--- README.md
+++ README.md
@@ -6,35 +6,48 @@
 1. [TOC (Table of Contents) generator](#toc-table-of-contents-generator)
     1. [Table of contents](#table-of-contents)
     2. [Description](#description)
-    3. [Usage](#usage)
-    4. [Programming language](#programming-language)
-    5. [Input and Output](#input-and-output)
-        1. [Input](#input)
-        2. [Output](#output)
-    6. [Docker](#docker)
+    3. [Programming language](#programming-language)
+    4. [Usage](#usage)
+        1. [Provide URL on command line](#provide-url-on-command-line)
+            1. [Input and Output](#input-and-output)
+                1. [Input](#input)
+                2. [Output](#output)
+        2. [Run as webserver](#run-as-webserver)
+    5. [Docker](#docker)
 <!-- GENERATED TOC -->
 
 ## Description
 Generates the table of contents for a markdown file, based on its headings.
+
+## Programming language
+Writen in GO
 
 ## Usage
 ```bash
 git clone https://github.com/mehiX/ReadmeTOC.git
 cd ReadmeTOC
 go build
-./ReadmeTOC FILEPATH | URL
 ```
 
-## Programming language
-Writen in GO
+### Provide URL on command line
+```bash
+./ReadmeTOC -path URL
+```
 
-## Input and Output
+#### Input and Output
 
-### Input
+##### Input
 Receives a url or file path to a Markdown document. More help with `-help`
 
-### Output
+##### Output
 Prints out the table of contents. This can be then pasted inside the original file.
+
+
+### Run as webserver
+```bash
+# Listen on 0.0.0.0:8080
+./ReadmeTOC -serve :8080
+```
 
 ## Docker
 
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/mehiX/ReadmeTOC/toc"
 )
 
 var (
-	help = flag.Bool("help", false, "Print this message")
+	help  = flag.Bool("help", false, "Print this message")
+	serve = flag.String("serve", "", "Start a webserver on the specified address")
+	path  = flag.String("path", "", "File path or URL to a markdown document")
 )
 
 /*
@@ -25,8 +31,8 @@ func init() {
 
 	flag.Parse()
 
-	if *help || len(flag.Args()) != 1 {
-		fmt.Fprintf(os.Stdout, "Usage: %s [-help] FILEPATH | URL\n", os.Args[0])
+	if *help || ("" == *serve && "" == *path) {
+		fmt.Fprintf(os.Stdout, "Usage: %s [-help] -serve ADDR -path URL\n", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
@@ -35,11 +41,70 @@ func init() {
 
 func main() {
 
-	url := flag.Arg(0)
+	if "" != *serve {
+		startServer()
+	} else {
+		generator := toc.NewGenerator(*path)
+
+		generator.Generate()
+
+		fmt.Fprintln(os.Stdout, generator.ToC)
+	}
+}
+
+func startServer() {
+
+	handler := mux.NewRouter()
+
+	handler.HandleFunc("/", handleQueryParam).Methods(http.MethodGet)
+	handler.HandleFunc("/json", handleJSON).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(*serve, handler))
+}
+
+func handleQueryParam(w http.ResponseWriter, r *http.Request) {
+
+	url := r.URL.Query().Get("path")
+
+	if "" == url {
+		http.Error(w, "Missing query parameter: path", http.StatusBadRequest)
+		return
+	}
+
 	generator := toc.NewGenerator(url)
 
 	generator.Generate()
 
-	fmt.Fprintln(os.Stdout, generator.ToC)
+	w.Header().Set("Content-type", "text/plain")
+	fmt.Fprint(w, generator.ToC)
+}
+
+type data struct {
+	URL string `json:"url"`
+	Toc string `json:"toc"`
+}
+
+func handleJSON(w http.ResponseWriter, r *http.Request) {
+
+	var d data
+
+	if err := json.NewDecoder(r.Body).Decode(&d); nil != err {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer r.Body.Close()
+
+	generator := toc.NewGenerator(d.URL)
+
+	generator.Generate()
+
+	d.Toc = generator.ToC
+
+	w.Header().Set("Content-type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(d); nil != err {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 
 }
